ede8b6299ef23dda4fa6ec9a087e430a4b9dc972
Syer10 <syer10@users.noreply.github.com>
2024-02-06 18:25:01 -0500

    Use import alias for Bitmap

--- bitmap/src/androidMain/kotlin/dev/mihon/bitmap/Bitmap.kt
+++ bitmap/src/androidMain/kotlin/dev/mihon/bitmap/Bitmap.kt
@@ -4,30 +4,31 @@ import android.annotation.TargetApi
 import android.os.Build
 import kotlinx.io.Sink
 import kotlinx.io.asOutputStream
+import android.graphics.Bitmap as NativeBitmap
 
-actual class Bitmap(val image: android.graphics.Bitmap) {
+actual class Bitmap(val image: NativeBitmap) {
     actual val width: Int = image.width
     actual val height: Int = image.height
 
-    actual enum class CompressFormat(val native: android.graphics.Bitmap.CompressFormat) {
-        JPEG(android.graphics.Bitmap.CompressFormat.JPEG),
-        PNG(android.graphics.Bitmap.CompressFormat.PNG),
+    actual enum class CompressFormat(val native: NativeBitmap.CompressFormat) {
+        JPEG(NativeBitmap.CompressFormat.JPEG),
+        PNG(NativeBitmap.CompressFormat.PNG),
         @TargetApi(Build.VERSION_CODES.R)
-        WEBP_LOSSY(android.graphics.Bitmap.CompressFormat.WEBP_LOSSY),
+        WEBP_LOSSY(NativeBitmap.CompressFormat.WEBP_LOSSY),
         @TargetApi(Build.VERSION_CODES.R)
-        WEBP_LOSSLESS(android.graphics.Bitmap.CompressFormat.WEBP_LOSSLESS)
+        WEBP_LOSSLESS(NativeBitmap.CompressFormat.WEBP_LOSSLESS)
     }
 
-    actual enum class Config(val native: android.graphics.Bitmap.Config) {
-        ALPHA_8(android.graphics.Bitmap.Config.ALPHA_8),
-        RGB_565(android.graphics.Bitmap.Config.RGB_565),
-        ARGB_8888(android.graphics.Bitmap.Config.ARGB_8888),
+    actual enum class Config(val native: NativeBitmap.Config) {
+        ALPHA_8(NativeBitmap.Config.ALPHA_8),
+        RGB_565(NativeBitmap.Config.RGB_565),
+        ARGB_8888(NativeBitmap.Config.ARGB_8888),
         @TargetApi(Build.VERSION_CODES.O)
-        RGBA_F16(android.graphics.Bitmap.Config.RGBA_F16),
+        RGBA_F16(NativeBitmap.Config.RGBA_F16),
         @TargetApi(Build.VERSION_CODES.O)
-        HARDWARE(android.graphics.Bitmap.Config.HARDWARE),
+        HARDWARE(NativeBitmap.Config.HARDWARE),
         @TargetApi(Build.VERSION_CODES.TIRAMISU)
-        RGBA_1010102(android.graphics.Bitmap.Config.RGBA_1010102);
+        RGBA_1010102(NativeBitmap.Config.RGBA_1010102);
     }
 
     actual fun compress(format: CompressFormat, quality: Int, sink: Sink): Boolean {
@@ -53,11 +54,11 @@ actual class Bitmap(val image: android.graphics.Bitmap) {
 
     actual companion object {
         actual fun createBitmap(width: Int, height: Int, config: Config): Bitmap {
-            return Bitmap(android.graphics.Bitmap.createBitmap(width, height, config.native))
+            return Bitmap(NativeBitmap.createBitmap(width, height, config.native))
         }
 
         actual fun createBitmap(source: Bitmap, x: Int, y: Int, width: Int, height: Int): Bitmap {
-            return Bitmap(android.graphics.Bitmap.createBitmap(source.image, x, y, width, height))
+            return Bitmap(NativeBitmap.createBitmap(source.image, x, y, width, height))
         }
     }
 }
