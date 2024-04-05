b9ddcfdae6dd60b9fb25689cd626f5eabca52870
AntsyLich <59261191+AntsyLich@users.noreply.github.com>
2024-04-05 19:16:12 +0600

    Actually fix build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,5 @@
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 pluginManagement {
-    resolutionStrategy {
-        eachPlugin {
-            if (requested.id.id == "com.android.library") {
-                useModule("com.android.tools.build:gradle:${requested.version}")
-            }
-        }
-    }
     repositories {
         google()
         gradlePluginPortal()
