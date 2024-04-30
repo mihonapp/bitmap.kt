20d882bafbcc6175ace681e1194663e9f9400b5e
AntsyLich <59261191+AntsyLich@users.noreply.github.com>
2024-04-30 20:03:56 +0600

    chore: preparation for snapshot release

--- bitmap/build.gradle.kts
+++ bitmap/build.gradle.kts
@@ -1,6 +1,9 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.maven.publish)
     alias(libs.plugins.spotless)
 }
 
@@ -64,4 +67,61 @@ spotless {
         trimTrailingWhitespace()
         endWithNewline()
     }
+}
+
+mavenPublishing {
+    val isSnapshot = System.getenv("SNAPSHOT").toBoolean()
+
+    val version = "0.0.0"
+    val developmentVersion = "1.0.0"
+
+    coordinates(
+        groupId =  "dev.mihon",
+        artifactId =  "bitmap-kt",
+        version = if (isSnapshot) "$developmentVersion-SNAPSHOT" else version
+    )
+
+    pom {
+        name.set("Bitmap.kt")
+        description.set("A Kotlin Multiplatform image manipulation library based on Android's Bitmap.")
+        url.set("https://github.com/mihonapp/bitmap.kt")
+        inceptionYear.set("2024")
+
+        licenses {
+            license {
+                name.set("Mozilla Public License Version 2.0")
+                url.set("https://www.mozilla.org/en-US/MPL/2.0")
+                distribution.set("repo")
+            }
+        }
+
+        organization {
+            organization {
+                name.set("The Mihon Open Source Project")
+                url.set("https://github.com/mihonapp")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("antsylich")
+                name.set("AntsyLich")
+                url.set("https://github.com/AntsyLich")
+            }
+
+            developer {
+                id.set("syer10")
+                name.set("Syer10")
+                url.set("https://github.com/Syer10")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/mihonapp/bitmap.kt")
+            connection.set("scm:git:git://github.com/mihonapp/bitmap.kt.git")
+        }
+    }
+
+    signAllPublications()
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = isSnapshot)
 }
